3b67095ea7bd1339a6a592444ab11f34c7d1aa31
Adam Howell <AdamJHowell@users.noreply.github.com>
2022-11-10 01:06:38 -0700

    Initial commit

---
+++ AverageReadings.ino
@@ -0,0 +1,53 @@
+#define LED_PIN 2                             // The GPIO that the onboard LED is attached to.
+unsigned long lastTelemetryPollTime = 0;	    // The last time sensors were polled.
+unsigned long telemetryPollInterval = 10000;  // How long to wait between sensor polling.
+float settingArray[] = { 0, 0, 0 };           // An array to hold the 3 most recent values.
+
+
+/**
+ * @brief readTelemetry() will manipulate the settingArray[] like a FIFO queue, by popping the head value off, and adding a new value to the tail.
+ */
+void readTelemetry()
+{
+  settingArray[0] = settingArray[1];
+  settingArray[1] = settingArray[2];
+  settingArray[2] = random( 99.9 );
+}
+
+
+void printTelemetry()
+{
+  float tempC = ( settingArray[0] + settingArray[1] + settingArray[2] ) / 3.0;
+  Serial.printf( "Average: %d", tempC );
+}
+
+
+void setup() 
+{
+ 	// This delay gives me time to open the serial console after flashing.
+	delay( 500 );
+
+	pinMode( LED_PIN, OUTPUT );
+	digitalWrite( LED_PIN, HIGH );
+
+	Serial.begin( 115200 );
+	if( !Serial )
+		delay( 1000 );
+	Serial.setDebugOutput( true );
+	Serial.println();
+
+	Serial.println( "setup() is running." );
+	Serial.printf( "Source file: %s\n", __FILE__ );
+}
+
+
+void loop() 
+{
+	unsigned long time = millis();
+	if( lastTelemetryPollTime == 0 || ( ( time > telemetryPollInterval ) && ( time - telemetryPollInterval ) > lastTelemetryPollTime ) )
+	{
+		readTelemetry();
+		printTelemetry();
+		lastTelemetryPollTime = millis();
+	}
+}
